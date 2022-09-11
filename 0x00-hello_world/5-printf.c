1807650e7127022db63ff07bc56506ad8157270f
1stohms <sinanureni@gmail.com>
2022-09-10 19:01:33 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Enter
+ *
+ * Return: Always 0 Great)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
