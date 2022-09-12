ddfbe0478aadd5dc0d02690cafb37f3ab13cf9aa
1stohms <sinanureni@gmail.com>
2022-09-12 02:13:09 -0700

    UNIX is basically a simple

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - printing string in put function
+ * main - Enter
  *
- * Description: using main function
- * Return: Always 0 (Good)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned)sizeof(long long int));
-	printf("Size of float: %lu byte(s)\n", (unsigned)sizeof(float));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof());
+	printf("Size of float: %lu byte(s)\n", (unsigned)sizeof(l));
 	return (0);
 }
