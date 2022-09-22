e7feb7ebb93475f6b8844543772f6bf8f18ada29
1stohms <sinanureni@gmail.com>
2022-09-21 17:31:21 -0700

    function that concatenates two strings.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,27 +1,30 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
 /**
- * File: main.h
- * Auth: alx
- * Desc: Prototype for all function
- * 	used in 0x06.C - more pointers, arrays and strings
+ *_strcat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will also be changed
+ *Return: dest
  */
 
-void reverse_array(int *a, int n);
-void print_number(int n);
-void print_buffer(char *b, int size);
-int _strcmp(char *s1, char *s2);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *_strcat(char *dest, char *src)
+{
+int i, j;
 
-#endif /*MAIN_H*/
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
 
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+dest[i] = '\0';
 
+return (dest);
+}
