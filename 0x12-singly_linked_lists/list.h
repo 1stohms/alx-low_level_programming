3ec4b1ba76e6697a9c3bca581decbbc60fd78c16
sina <111012499+1stohms@users.noreply.github.com>
2022-10-21 03:31:54 +0100

    Update list.h

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,5 +1,7 @@
-#ifndef _LINKED_LIST
-#define _LINKED_LIST
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -17,11 +19,11 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-int _strlen(const char *s);
+int _putchar(char);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /* _LISTS_H_ */
