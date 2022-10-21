a270d406f788ffdb2c2178b2234a88799fa2fc0d
1stohms <sinanureni@gmail.com>
2022-10-20 20:18:08 -0700

    0x12.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,16 +1,14 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: singly linked list node structure
- * 		for Holberton project
+ * Description: singly liked list node structure
+ *		for project
  */
 typedef struct list_s
 {
@@ -21,7 +19,7 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
