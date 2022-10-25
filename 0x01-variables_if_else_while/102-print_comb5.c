a871d8332d84d52505d7b481366ca3d074c1a65f
1stohms <sinanureni@gmail.com>
2022-10-25 00:44:29 -0700

    print

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return: alway returns 0
-*/
+ * Return: Always success
+ */
 int main(void)
 {
-	int i;
-	int j;
 
-	for (i = 0; i <= 99; i++)
-	{
-		for (j = i + 1; j <= 99; j++)
-		{
-			putchar(i / 10 + '0');
-			putchar(i % 10 + '0');
-			putchar(' ');
-			putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
-			
-			if (!(i == 98 && j == 99))
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+int num1, num2;
+
+for (num1 = 0; num1 <= 98; num1++)
+
+{
+
+for (num2 = num1 + 1; num2 <= 99; num2++)
+
+{
+
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
+putchar(' ');
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+
+if (num1 == 98 && num2 == 99)
+
+continue;
+
+putchar(',');
+putchar(' ');
+
+}
+
+}
+
+putchar('\n');
+
+return (0);
 }
