0c8a982560e3bf673575899c1e26b3a26d0367a6
sina <111012499+1stohms@users.noreply.github.com>
2022-10-26 11:22:19 +0100

    1 update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -6,7 +6,6 @@
  * print_numbers - prints numbers, followed by a new line.
  * @separator: separator to print between numbers
  * @n: number of numbers to print
- *
  * Return: void
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
