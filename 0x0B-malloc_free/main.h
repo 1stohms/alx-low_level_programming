80b7fd9882df8fc1bb9878ab73d8299614ffcf4b
sina <111012499+1stohms@users.noreply.github.com>
2022-11-02 15:21:54 +0100

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,13 +1,13 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
-char *_strdup(char *str);
-char **strtow(char *str);
-char *argstostr(int ac, char **av);
-char *str_concat(char *s1, char *s2);
-void free_grid(int **grid, int height);
-int **alloc_grid(int width, int height);
-char *create_array(unsigned int size, char c);
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-#endif
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /* MAIN_H */
